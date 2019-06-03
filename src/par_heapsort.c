5da00e3e11e7f10acf3539a969786d6bbe651604
MQuaresma <miguelquaresma97@gmail.com>
2019-06-03 22:02:13 +0100

    Parallel version with heap level granularity but race conditions

--- src/par_heapsort.c
+++ src/par_heapsort.c
@@ -3,68 +3,83 @@
 #include "utils.c"
 #include <omp.h>
 #include <pthread.h>
+#include <math.h>
+#include <string.h>
 
 void heap_sort();
 void heapify(int a[], int);
 void shiftDown(int a[], int, int);
+void shiftDown_seq(int a[], int start, int end);
 
 static int *a;
 static int size;
 static int end;
 static int N_threads;
+static int level_count;
+static pthread_mutex_t *level_mutexes; 
+static pthread_mutex_t end_mut;
 
-void *aux_parallel(void *id){
-  while ( end>0 ) { // do in parallel
-    // do in sequential order
-    int cmax = a[0];
-    a[0]=a[end];
-    a[end]=cmax; //swap
-    end--; // decrease heap size
-    // repair heap : do with locking of parent and child
-    shiftDown(a,0,end);
-  }
+void *aux_parallel_level(void *id){
+    int max;
+    pthread_mutex_lock(&end_mut);
+
+    while(end > 0){
+        pthread_mutex_lock(level_mutexes);
+        pthread_mutex_lock(level_mutexes+level_count);
+        max = a[0];
+        a[0] = a[end];
+        a[end] = max;
+        pthread_mutex_unlock(level_mutexes);
+        pthread_mutex_unlock(level_mutexes+level_count);
+        end--;
+        shiftDown(a,0,end);
+        pthread_mutex_lock(&end_mut);
+    }
+
+    pthread_mutex_unlock(&end_mut);
+    return NULL;
 }
 
-//--------- skeleton code for parallel version with locks
 void heap_sort() {
-  heapify(a,size); // build max-heap (sequential)
+    //level_count = log(size) / log(2);
+    level_count = 4;
+    pthread_t* thread_handles = (pthread_t*)malloc(N_threads*sizeof(pthread_t));
 
-  end=size-1;
+    heapify(a,size); 
+    level_mutexes = (pthread_mutex_t*)malloc(level_count * sizeof(pthread_mutex_t));
+    pthread_mutex_init(&end_mut, NULL);
+    end = size - 1;
 
-  pthread_t* thread_handles = (pthread_t*)malloc(N_threads*sizeof(pthread_t));
+    for(int i=0; i < level_count; i ++)
+        pthread_mutex_init(level_mutexes+i, NULL);
 
-  for(long i=0; i<N_threads; i++)
-        pthread_create(&thread_handles[i],NULL,aux_parallel, (void *)i);
+    for(long i=0; i<N_threads; i++)
+        pthread_create(&thread_handles[i], NULL, aux_parallel_level, (void *)i);
 
-  for(long i=0; i<N_threads; i++)
-        pthread_join(thread_handles[i],NULL);
+    for(long i=0; i<N_threads; i++)
+        pthread_join(thread_handles[i], NULL);
 
-  free(thread_handles);
+    pthread_mutex_destroy(&end_mut);
+    for(int i=0; i < level_count; i ++)
+        pthread_mutex_destroy(level_mutexes+i);
+
+    free(level_mutexes);
+    free(thread_handles);
 }
 
-void heapify(int a[], int count) {
-  int start=(count-1)/2;
-
-  while(start>=0) {
-    shiftDown(a,start,count-1);
-    start--;
-  }
-}
-
-void shiftDown(int a[], int start, int end) {
+void shiftDown_seq(int a[], int start, int end) {
   int root = start;
   
   while( (2*root+1) <= end) {
-    int child = LEFT(root);
+    int child = 2*root+1;
     int swap = root;
     
     if (a[swap]<a[child]) swap = child;
     if ((child+1) <= end)
-      if (a[swap]<a[child+1]) swap = RIGHT(root);
-    if(swap==root) root=end; // return
+      if (a[swap]<a[child+1]) swap = child+1;
+    if(swap==root) root=end;
     else {
       int tmp = a[swap];
-      
       a[swap]=a[root];
       a[root]=tmp;
       root = swap;
@@ -72,8 +87,53 @@ void shiftDown(int a[], int start, int end) {
   }
 }
 
+
+
+void shiftDown(int a[], int start, int end_a) {
+    pthread_mutex_unlock(&end_mut);
+    int root = start;
+    int child;
+    int swap;
+    int tmp;
+    int cur_level = 0;
+  
+    while( (2*root+1) <= end_a) {
+        child = LEFT(root);
+        swap = root;
+        pthread_mutex_lock(level_mutexes+cur_level);
+        pthread_mutex_lock(level_mutexes+cur_level+1);
+
+        if (a[swap]<a[child]) 
+            swap = child;
+        if ((child+1) <= end_a)
+            if (a[swap]<a[child+1]) swap = child+1;
+        if(swap==root) 
+            root = end_a;
+        else {
+            tmp = a[swap];
+            a[swap] = a[root];
+            a[root] = tmp;
+            root = swap;
+        }
+        pthread_mutex_unlock(level_mutexes+cur_level);
+        pthread_mutex_unlock(level_mutexes+cur_level+1);
+        cur_level ++;
+    }
+}
+
+void heapify(int a[], int count) {
+  int start=(count-1)/2;
+
+  while(start>=0) {
+    shiftDown_seq(a,start,count-1);
+    start--;
+  }
+}
+
+
+
 int main(int argc, char *argv[]){
-    if(argc == 4){
+    if(argc >= 4){
         size = atoi(argv[1]);
         N_threads = atoi(argv[2]);
         a = (int *)calloc(size, sizeof(int));
@@ -89,8 +149,15 @@ int main(int argc, char *argv[]){
         end = omp_get_wtime();
 
         printf("Time: %f\n", end - start);
+        if(argc >= 5)
+            if(!strcmp(argv[4], "-d"))
+                pprint_array(a, size);
+            if(is_sorted(a, size))
+                printf("\nSorted!\n");
+            else
+                printf("\nSomething went wrong!\n");
     }else{
-        printf("USAGE: %s size threads exclusionZones\n", argv[0]);
+        printf("USAGE: %s size threads exclusionZones [-d]\n", argv[0]);
     }
     return 0;
 }
